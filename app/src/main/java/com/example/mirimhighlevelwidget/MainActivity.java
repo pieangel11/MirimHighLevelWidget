c190bc8fab475f05926ee6a1be56144194d61eb2
young <pieangel11@naver.com>
2020-11-02 19:38:27 +0900

    Complete First Reservation System

--- app/src/main/java/com/example/mirimhighlevelwidget/MainActivity.java
+++ app/src/main/java/com/example/mirimhighlevelwidget/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.mirimhighlevelwidget;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Chronometer;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton radioDate, radioTime;
     CalendarView calendar1;
     TimePicker timePick;
+    Chronometer chrono;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         radioTime.setOnClickListener(radioListener);
         calendar1 = findViewById(R.id.calendar1);
         timePick = findViewById(R.id.time_pick);
+        chrono = findViewById(R.id.chrono1);
+        textResult = findViewById(R.id.text_result);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnStop = findViewById(R.id.btn_stop);
+        btnStart.setOnClickListener(btnListener);
+        btnStop.setOnClickListener(btnListener);
+        calendar1.setOnDateChangeListener(calendarListener);
     }
     View.OnClickListener radioListener = new View.OnClickListener() {
         @Override
@@ -37,6 +52,33 @@ public class MainActivity extends AppCompatActivity {
                     timePick.setVisibility(View.VISIBLE);
                     break;
             }
+        }
+    };
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.btn_start:
+                    chrono.setBase(SystemClock.elapsedRealtime()); // avd에 있는 시간을 이용하여 timer를 시작한다.
+                    chrono.start();
+                    chrono.setTextColor(Color.RED);
+                    break;
+                case R.id.btn_stop:
+                    chrono.stop();
+                    chrono.setTextColor(Color.BLUE);
+                    textResult.setText(dateStr + timePick.getCurrentHour() + "시 " + timePick.getCurrentMinute() + "분");
+                    break;
+            }
+        }
+    };
+
+    String dateStr = "";
+
+    CalendarView.OnDateChangeListener calendarListener = new CalendarView.OnDateChangeListener() {
+        @Override
+        public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+            dateStr = year + "년 " + (month + 1) + "월 " + dayOfMonth + "일 ";
         }
     };
 }
